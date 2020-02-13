5ccf0688684125eef869cdd2a0fb31e80be5abaa
recode-lab <studio@recodelab.com>
2020-02-13 11:12:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Meine MiniLAB Seite</title>
 </head>
 <body>
+    
+    <?php 
+while( have_posts() ) : the_post();
+   the_title();
+    the_content();
+endwhile;
+?>
 
 </body>
 </html>
