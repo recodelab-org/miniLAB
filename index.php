2c4e6db67058a30c92d98e4acb4149572fbc0605
recode-lab <studio@recodelab.com>
2020-02-13 14:57:43 +0100

    Updated index and stylesheet

--- index.php
+++ index.php
@@ -1,29 +1,41 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title><?php bloginfo('title'); ?></title>
     <?php wp_head() ?>
 </head>
+
 <body <?php body_class(); ?>>
-    
+
     <nav>
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></nav>
-    
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    </nav>
+
     <h1><?php bloginfo('title'); ?></h1>
- 
-<!--    Dies ist der berühmte Loop-->
-    <?php 
-while( have_posts() ) : the_post();
-   the_title();
-    the_content();
-endwhile;
-?>
-    
+
+    <main>
+        <article>
+
+            <!--Dies ist der berühmte Loop-->
+            <?php 
+            while( have_posts() ) : the_post(); ?>
+
+
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+
+
+            <?php endwhile; ?>
+        </article>
+    </main>
+
     <footer>
         <nav>
-        <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?></nav>
-    
+            <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+        </nav>
     </footer>
 
 </body>
+
 </html>
