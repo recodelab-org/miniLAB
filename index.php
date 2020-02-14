e700c504be114fa1b143693c0e078eadf37b3c10
recode-lab <studio@recodelab.com>
2020-02-14 11:13:36 +0100

    Multiple adjustmnents

--- index.php
+++ index.php
@@ -19,21 +19,21 @@
 
         <!--The Loop-->
         <?php while( have_posts() ) : the_post(); ?>
-        
+
         <article>
-            
+
             <span class="image my-featured-image">
                 <?php echo the_post_thumbnail()?>
             </span>
 
             <header class="major">
                 <h3><a href="<?php echo esc_url( get_permalink() ); ?>" class="link"><?php the_title(); ?></a></h3>
-                <p><?php the_excerpt(); ?></p>
-                <!--<?php the_content(); ?>-->
+                <!--<p><?php the_excerpt(); ?></p>-->
+                <?php the_content(); ?>
             </header>
 
         </article>
-        
+
         <?php endwhile; ?>
         <!--Loop ends-->
 
