d204855546ddb060ed5797eaa593913f8509ce41
Enno Hyttrek <ehy.global@gmail.com>
2021-04-29 11:57:32 +0200

    Version 1.2.3
    Added fonts Inter and Merriweather. Defined Inter as new title font and Merriweather as new body font. Added CSS variables to set title font and body font in style.css

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ function minilab_stylesheets() {
     wp_enqueue_style( 'style', get_stylesheet_uri()); 
     wp_enqueue_style( 'roboto-font', get_template_directory_uri() . '/fonts/roboto/font-style.css' );
     wp_enqueue_style( 'petrona-font', get_template_directory_uri() . '/fonts/petrona/font-style.css' );
+    wp_enqueue_style( 'inter-font', get_template_directory_uri() . '/fonts/inter/font-style.css' );
+    wp_enqueue_style( 'merriweather-font', get_template_directory_uri() . '/fonts/merriweather/font-style.css' );
     wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/fonts/fontawesome/css/all.css' );
 }
 add_action( 'wp_enqueue_scripts', 'minilab_stylesheets' );
